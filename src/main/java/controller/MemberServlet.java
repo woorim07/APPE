9f0a9331a5f52894c63266b71dffc00f45e14750
고우림 <ji232347@gmail.com>
2025-08-21 09:28:12 +0900

    4. 서블릿 구현 및 WebServlet 적용

--- src/main/java/controller/MemberServlet.java
+++ src/main/java/controller/MemberServlet.java
@@ -1,41 +1,31 @@
 package controller;
 
+import model.MemberDAO;
+import model.MemberDTO;
 import java.io.IOException;
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 
-/**
- * Servlet implementation class MemberServlet
- */
 @WebServlet("/MemberServlet")
 public class MemberServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MemberServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+
+        String username = request.getParameter("username");
+        String userid = request.getParameter("userid");
+        String password = request.getParameter("password");
+        String email = request.getParameter("email");
+
+        MemberDTO member = new MemberDTO();
+        member.setUsername(username);
+        member.setUserid(userid);
+        member.setPassword(password);
+        member.setEmail(email);
+
+        MemberDAO dao = new MemberDAO();
+        dao.insertMember(member);
+
+        response.sendRedirect("memberList.jsp");
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
